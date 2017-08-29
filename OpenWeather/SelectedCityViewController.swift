e4d3cad4a87420187c5c8ea4d96f9781156b1375
Christopher Rene <roderick540@gmail.com>
2017-08-29 18:46:28 -0400

    Setup detail views for selected cities
    still needs some work to do

---
+++ OpenWeather/SelectedCityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SelectedCityViewController.swift
+//  OpenWeather
+//
+//  Created by Christopher Rene on 8/29/17.
+//  Copyright © 2017 Fauna. All rights reserved.
+//
+
+import UIKit
+
+class SelectedCityViewController: UIViewController {
+
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var rainLabel: UILabel!
+    @IBOutlet weak var windLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismiss(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+
+}
