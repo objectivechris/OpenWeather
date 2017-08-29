5775427485c144694a024fae51613192236cae80
Christopher Rene <roderick540@gmail.com>
2017-08-29 16:36:43 -0400

    Creating initial setup in storyboard

---
+++ OpenWeather/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  OpenWeather
+//
+//  Created by Christopher Rene on 8/29/17.
+//  Copyright © 2017 Fauna. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
