e7dfbed0f7e5cfdf3a018da074e10089a1485f6c
Christopher Rene <roderick540@gmail.com>
2017-08-30 02:58:37 -0400

    Implemented logic for adding location data

---
+++ OpenWeather/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  OpenWeather
+//
+//  Created by Christopher Rene on 8/30/17.
+//  Copyright © 2017 Fauna. All rights reserved.
+//
+
+import Foundation
--- OpenWeather/PinLocationViewController.swift
+++ OpenWeather/PinLocationViewController.swift
@@ -9,35 +9,101 @@
 import UIKit
 import MapKit
 
+protocol PinLocationViewControllerDelegate: class {
+    func didSelectLocation(_ location: SelectedLocation)
+}
+
 class PinLocationViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var labelBackground: UIView!
+    @IBOutlet weak var buttonBackground: UIView!
+    
+    weak var delegate: PinLocationViewControllerDelegate?
+    
+    var currentLocation = CLLocation(latitude: 0, longitude: 0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Show user location in sepcified region
+        let region = MKCoordinateRegionMakeWithDistance(currentLocation.coordinate, CLLocationDistance(500), CLLocationDistance(500))
+        mapView.showsUserLocation = true
+        mapView.setRegion(region, animated: true)
+        
+        let holdTapGesture = UILongPressGestureRecognizer(target: self, action: #selector(selectLocation))
+        holdTapGesture.minimumPressDuration = 0.5
+        mapView.addGestureRecognizer(holdTapGesture)
+        
+        labelBackground.addDropShadow()
+        buttonBackground.addDropShadow()
     }
     
-    @IBAction func dismiss(_ sender: Any) {
+    func selectLocation(gesture: UILongPressGestureRecognizer) {
         
+        if gesture.state == .began {
+            
+            let touchLocation = gesture.location(in: mapView)
+            let touchPoint = mapView.convert(touchLocation, toCoordinateFrom: self.view)
+            
+            // Begin network request
+            OpenWeatherMapClient().getCurrentWeather(for: touchPoint, completion: { (status) in
+                
+                typealias JSONDictionary = [String: Any]
+                
+                switch status {
+                    
+                case let .successful(data):
+                    
+                    guard let dict = data as? JSONDictionary else { return }
+                
+                    let city = dict["name"] as! String
+                    let latitude = (dict["coord"] as? JSONDictionary)?["lat"] as! Double
+                    let longitude = (dict["coord"] as? JSONDictionary)?["lon"] as! Double
+                    let temperature = (dict["main"] as? JSONDictionary)?["temp"] as! Double
+                    let humidity = (dict["main"] as? JSONDictionary)?["humidity"] as! Double
+                    let rain = (dict["rain"] as? JSONDictionary)?["3h"] as? Double
+                    let wind = (dict["wind"] as? JSONDictionary)?["speed"] as! Double
+                    
+                    // Create location object from parsed JSON data
+                    let selectionLocation = SelectedLocation(city: city, temperature: temperature, humidity: humidity, rain: rain, wind: wind, latitude: latitude, longitude: longitude)
+                    
+                    // Create and drop pin where user touched
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = touchPoint
+                    
+                    // Confirm pin annotation & alert delegate
+                    let alert = UIAlertController(title: "Add Location", message: "Do you want to add this location as a bookmark?", preferredStyle: .alert)
+                    let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+                    let okay = UIAlertAction(title: "Yes", style: .default, handler: { (action) in
+                        self.delegate?.didSelectLocation(selectionLocation)
+                        self.mapView.addAnnotation(annotation)
+                        
+                        let later = DispatchTime.now() + 1
+                        DispatchQueue.main.asyncAfter(deadline: later) {
+                            // delay dismissal after pin drops
+                            self.dismiss(animated: true, completion: nil)
+                        }
+                    })
+                    
+                    alert.addAction(cancel)
+                    alert.addAction(okay)
+                    
+                    DispatchQueue.main.async {
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                    
+                case let .error(error):
+                    let alert = UIAlertController(title: "Uh Oh", message: "Something bad happened - \(error)", preferredStyle: .alert)
+                    let okay = UIAlertAction(title: "OK", style: .default, handler: nil)
+                    alert.addAction(okay)
+                    self.present(alert, animated: true, completion: nil)
+                }
+            })
+        }
+    }
+
+    @IBAction func dismiss(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
